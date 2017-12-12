f8af6b4594e73715bfdd93ae8e33ba1488943830
Sandy <sav3ae@virginia.edu>
2017-12-11 23:21:14 -0800

    Update methods in board class

---
+++ W2D2/lib/board.rb
@@ -0,0 +1,69 @@
+class Board
+  attr_accessor :cups
+
+  def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2
+    @cups = Array.new(14) { Array.new }
+    place_stones
+  end
+
+  def place_stones
+    # helper method to #initialize every non-store cup with four stones each
+
+
+    @cups.each_with_index do |cup, idx|
+      unless idx == 6 || idx == 13
+        4.times do
+          cup << :stone
+        end
+      end
+    end
+  end
+
+  def valid_move?(start_pos)
+    raise "Invalid starting cup" if start_pos < 0 || start_pos > 13
+    raise "Invalid starting cup" if @cups[start_pos].empty?
+  end
+
+  def make_move(start_pos, current_player_name)
+    @cups[start_pos] = []
+
+  end
+
+  def next_turn(ending_cup_idx)
+    # helper method to determine what #make_move returns
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt
+    elsif @cups[ending_cup_idx].count == 1
+      :switch
+    else
+      ending_cup_idx
+    end
+  end
+
+  def render
+    print "      #{@cups[7..12].reverse.map { |cup| cup.count }}      \n"
+    puts "#{@cups[13].count} -------------------------- #{@cups[6].count}"
+    print "      #{@cups.take(6).map { |cup| cup.count }}      \n"
+    puts ""
+    puts ""
+  end
+
+  def one_side_empty?
+    @cups[0..5].all? { |cup| cup.empty? } || @cups[7..12].all? { |cup| cup.empty? }
+  end
+
+  def winner
+    player1_stones = @cups[6].length
+    player2_stones = @cups[13].length
+
+    if player1_stones == player2_stones
+      :draw
+    elsif player1_stones > player2_stones
+      @name1
+    else
+      @name2
+    end
+  end
+end
